f88d1951d48ece78ca8591bd63d026bc79afccee
onurkaraoglan <okaraoglan91@gmail.com>
2019-05-31 17:07:35 +0300

    first blog post added

--- src/components/index/blog.js
+++ src/components/index/blog.js
@@ -10,13 +10,15 @@ const Blog = () => (
     <div>
       <Row gutter={16} type="flex">
         <Col span={8} className="card">
+        <div onClick={()=> window.open("https://medium.com/@onurkaraoglan/big-o-notation-with-code-examples-302f999dec40", "_blank")}>
           <BlogCard
-            title="title 1"
-            description="description 1"
-            imgsrc="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-            mediumlink="https://medium.com/@onurkaraoglan"
+            title="Big O Notation"
+            description="Big O notation is used in Computer Science to describe the performance or complexity of an algorithm. "
+            imgsrc="https://res.cloudinary.com/onurkaraoglan/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1559310567/blog/Ekran_Resmi_2019-05-31_14.26.22_rhf0id.png"
+            mediumlink="https://medium.com/@onurkaraoglan/big-o-notation-with-code-examples-302f999dec40"
 
           />
+          </div>
         </Col>
         <Col span={8} className="card">
           <BlogCard
